3353e6101ba69c1e3fe473bda0ce596515a77326
Indumathi-V <54302074+Indumathi-V@users.noreply.github.com>
2026-09-23 10:55:03 +0530

    Create reducer.js

---
+++ src/reducer.js
@@ -0,0 +1,14 @@
+// TODO: Create the reducer function for the shopping cart
+
+function cartReducer(state, action) {
+  // TODO:
+  // Handle the following actions:
+  // ADD
+  // INCREASE
+  // DECREASE
+  // REMOVE
+
+  return state;
+}
+
+export default cartReducer;
